dfa382b0e9b0a7f52b6ce498c2c414db4fca1ad9
user <joshmanuel2016@gmail.com>
2023-09-12 15:27:31 +0800

    leaflet and geolocation intro

---
+++ script.js
@@ -0,0 +1,21 @@
+const map = L.map("map").setView([0, 0], 2);
+
+L.tileLayer("https://tile.openstreetmap.org/{z}/{x}/{y}.png", {
+  maxZoom: 19,
+  attribution:
+    '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>',
+}).addTo(map);
+
+// const marker = L.marker([51.5, -0.09]).addTo(map);
+
+const marker = L.marker([0, 0]).addTo(map);
+
+navigator.geolocation.getCurrentPosition(function (pos) {
+  const lat = pos.coords.latitude;
+  const lng = pos.coords.longitude;
+
+  marker.setLatLng([lat, lng]).update();
+  map.setView([lat, lng], 13);
+
+  marker.bindPopup("<strong>Hello World</strong>");
+});
